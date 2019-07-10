168ef49b572b0ba3b7acfe29913572cb02ab02b0
sunjohanday <santeri.sydanmetsa@edu.savonia.fi>
2019-07-10 20:55:11 +0300

    install.sh compiles now with cpp

--- src/xander/install.sh
+++ src/xander/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 [ -d "~/.xander" ] && mkdir "~/.xander"
 printf "Compiling Xander..."
-~/.nimble/bin/nim c --verbosity:1 --hints:off -o:~/.xander/xander ~/.nimble/pkgs/xander-0.5.0/xander.nim
+# Compiling with cpp for better compatiblity
+~/.nimble/bin/nim cpp --verbosity:1 --hints:off -o:~/.xander/xander ~/.nimble/pkgs/xander-0.5.0/xander.nim
 echo "Finished. The Xander executable can be found in ~/.xander"
 echo "Add the following line to ~/.bashrc or ~/.profile:"
 echo ""
