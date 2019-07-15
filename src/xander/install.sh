a893e9144c8133439ae0984ab70d8f5f423d4eba
sunjohanday <santeri.sydanmetsa@edu.savonia.fi>
2019-07-15 15:41:04 +0300

    added colors to install.sh

--- src/xander/install.sh
+++ src/xander/install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+# Colors
+GREEN='\033[0;32m'
+WHITE='\033[1;37m'
+NC='\033[0m' # No Color
+# Make .xander directory
 [ -d "~/.xander" ] && mkdir "~/.xander"
 # Get latest version of Xander
 x=($(ls ~/.nimble/pkgs | grep xander-))
 y=~/.nimble/pkgs/${x[-1]}/xander.nim
 # Compiling with cpp for better compatiblity
 echo "Found Xander in $y"
-echo "Compiling ${x[-1]}"
+echo -e "${WHITE}Compiling ${x[-1]}${NC}"
 ~/.nimble/bin/nim cpp --verbosity:1 --hints:off --threads:on -o:~/.xander/xander $y
-echo "Finished. The Xander executable can be found in ~/.xander"
+echo -e "${GREEN}Finished!${NC} The Xander executable can be found in ~/.xander"
 echo "Add the following line to ~/.bashrc or ~/.profile:"
 echo ""
 echo "  export PATH=/home/$USER/.xander:\$PATH"
